d7243ffc81b07026978dc8d448a2c389a6a0807b
c1chy1 <cichy08081987pp@gmail.com>
2022-03-28 15:26:06 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: process.env.NODE_ENV ? 'jit' : undefined,
+
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
