7ea9d930ef858bacfdb9a9b83453420ea59d6d19
c1chy1 <cichy08081987pp@gmail.com>
2022-03-29 14:45:43 +0200

    store index #1

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,35 @@
 import { createStore } from 'vuex'
-import UserModule from '../store/modules/user-module'
+/*import UserModule from '../store/modules/user-module'*/
+import axios from 'axios'
+
+
 
 export default createStore({
   state: {
+
+    users: []
+
   },
   getters: {
+
+    usersList: state => state.users
+
   },
   mutations: {
 
-
+    setUsers: (state, users) => (
+        state.users = users)
 
   },
   actions: {
+
+    async fetchUsers({commit}) {
+      const response = await axios.get("http://localhost:3000/users");
+      commit("setUsers", response.data)
+    }
+
   },
   modules: {
 
-    UserModule
   }
 })
